eccb198bd7a795346b368432474789bb50923d87
ty <iamtylerwarren@gmail.com>
2017-05-04 17:53:32 -0700

    day 2 complete

--- day1/resources/js/main.js
+++ day1/resources/js/main.js
@@ -1,23 +1,54 @@
 'use strict';
 
+
 function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`section.key[data-key="${e.keyCode}"]`);
     // if a key doesnt have audio attached to it return
-    if(!audio) return; 
+    if(!audio) return;
     // rewinds sound to start
     audio.pause()
     audio.currentTime = 0;
     audio.play();
-    key.classList.add('playing');
+    transition(e,key)
 }
-function removeTransition(e){
-    clearTimeout()
-    if(e.propertyName !== 'transform') return;
+function transition(e,key){
+    key.classList.add('playing');
     setTimeout(function(){
-        this.classList.remove('playing');
+        key.classList.remove('playing');
     }.bind(this), 200);
 }
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
 window.addEventListener('keydown', playSound);
+
+function drumClick(){
+    const keyLetter = this.firstChild.nextSibling.innerHTML;
+    let sound = "";
+    switch(keyLetter){
+        case "S":
+            sound = 'kick'
+            break;
+        case "F":
+            sound = 'snare'
+            break;
+        case "H":
+            sound = 'openhat'
+            break;
+        case "K":
+            sound = 'hat'
+            break;
+        default:
+            break;
+    }
+    let audio = new Audio(`resources/sounds/${sound}.wav`)
+    audio.currentTime = 0;
+    audio.play();
+    this.classList.add('playing');
+    setTimeout(function(){
+        this.classList.remove('playing');
+    }.bind(this), 200)
+}
+const clickKeys = document.querySelectorAll('section.key');
+clickKeys.forEach(key => key.addEventListener('click', drumClick));
+
---
+++ day2/resources/js/main.js
@@ -0,0 +1,52 @@
+'use_strict';
+
+const secHand = document.querySelector('.secHand');
+const minHand = document.querySelector('.minHand');
+const hourHand = document.querySelector('.hourHand');
+const tick = new Audio('resources/sounds/tick.wav');
+let soundOn = false;
+
+function soundToggle(){
+    if(soundOn){
+        soundOn = false;
+    } else {
+        soundOn = true;
+    }
+    console.log(soundOn)
+}
+
+function playTick(){
+    tick.currentTime = 0;
+    tick.play()
+}
+function toDegrees(time){
+    let result = ((time / 60) * 360 + 90);
+    if(result == 444){
+        setTimeout(function(){
+            secHand.style.transition = "";
+        }, 1000);
+    }
+    return((time / 60) * 360 + 90)
+}
+
+function calcHours(hours){
+    return ((hours / 12) * 360 + 90);
+}
+
+
+function setDate(){
+    const now = new Date;
+    // const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+    // secHand.style.transform = `rotate(${toDegrees(seconds)}deg)`;
+    minHand.style.transform = `rotate(${toDegrees(minutes)}deg)`;
+    hourHand.style.transform = `rotate(${calcHours(hours)}deg)`;
+    if(soundOn){
+        playTick()
+    }
+}
+
+setInterval(setDate, 1000);
+window.addEventListener('click', soundToggle);
+
